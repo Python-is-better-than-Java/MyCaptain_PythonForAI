f819248edbfb67d02d5f7ea1f6c66e8582048f7a
Arya Umesh <74286505+Python-is-better-than-Java@users.noreply.github.com>
2023-01-22 13:02:04 +0530

    Created Data_Structures.py
    Displays the union, intersection, difference and symmetric difference between two sets

---
+++ Data_Structures.py
@@ -0,0 +1,10 @@
+# Operations on set - Union, Intersection, Difference, Symmetric Difference
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+print("A: ", A)
+print("B: ", B)
+print("Union of the two sets: ", A.union(B))
+print("Intersection of the two sets: ", A.intersection(B))
+print("Difference between the two sets: ", A.difference(B))
+print("Symmetric difference between the two sets: ", A.symmetric_difference(B))
